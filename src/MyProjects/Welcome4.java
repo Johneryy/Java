6b0c89027c417b0b9222893ea022b776bc200dca
Johneryy <johnakintolu4@gmail.com>
2022-06-22 13:37:13 +0800

    Java

--- src/MyProjects/Welcome4.java
+++ src/MyProjects/Welcome4.java
@@ -2,6 +2,6 @@ package MyProjects;
 
 public class Welcome4 {
     public static void main(String[] args) {
-        System.out.printf("%s%n%s%n", "Welcome to" , "Java Programming!");
+        System.out.printf("%s%n%s%n", "Welcome to" , "Java Programming!!");
     }
 }
